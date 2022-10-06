7337af035aaf2e8d5506615b08b5737ad7d6efc7
noy4 <noy4@users.noreply.github.com>
2022-10-06 09:54:56 +0000

    deploy: 37449ef89ebc1c094440d0c8c818b30aec8ddf9c

---
+++ _nuxt/_postId_.3e8ce772.js
@@ -0,0 +1,1 @@
+import{a,o as t,e as r,f as s,t as e}from"./entry.438ca6dc.js";const d={};function n(o,p){return t(),r("div",null,[s("div",null,"postid: "+e(o.$route.params.postid),1),s("div",null,"postId: "+e(o.$route.params.postId),1),s("div",null,"post-id: "+e(o.$route.params["post-id"]),1),s("div",null,e(o.$route.params),1)])}const i=a(d,[["render",n]]);export{i as default};
---
+++ _nuxt/index.8fec0136.js
@@ -0,0 +1,1 @@
+import{a as e,o as c,e as n}from"./entry.438ca6dc.js";const o={};function r(t,a){return c(),n("div",null,"Hey")}const _=e(o,[["render",r]]);export{_ as default};
